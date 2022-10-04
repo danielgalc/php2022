31a9afd3247eef25b60ee24073dc521a79eea369
danielgalc <daniel.gallego@iesdonana.org>
2022-10-04 20:54:19 +0200

    Hamming

---
+++ trabajo/hamming/hamming.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultado</title>
+</head>
+<body>
+    <?php
+    
+    $primera_cadena = $_GET['primera_cad'];
+    $segunda_cadena = $_GET['segunda_cad'];
+    
+    
+    ?>
+</body>
+</html>
