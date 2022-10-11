b0c79c99266ea624576de4accd526363772975b4
danielgalc <daniel.gallego@iesdonana.org>
2022-10-11 19:44:34 +0200

    Los parametros se mantienen

--- trabajo/calculadora/index.php
+++ trabajo/calculadora/index.php
@@ -13,46 +13,40 @@
     require 'auxiliar.php';
 
     $error = [];
-    if (isset($_GET['op1'])) {
-        $op1 = trim($_GET['op1']);
-    } else {
-        $error[] = 'Falta el parámetro op1';
+    
+    $op1 = isset($_GET['op1']) ? trim($_GET['op1']) : null;
+    $op2 = isset($_GET['op2']) ? trim($_GET['op2']) : null;
+    $op = isset($_GET['op']) ? trim($_GET['op']) : null;
+
+    
+    if (isset($op1, $op2, $op)) {
+        try{
+            $res = calcular_resultado($op1, $op2, $op);
+            mostrar_resultado($op1, $op2, $op, $res);
+        } catch (Exception $e){
+            mostrar_errores([$e->getMessage()]);
+        }
     }
-    if (isset($_GET['op2'])) {
-        $op2 = trim($_GET['op2']);
-    } else {
-        $error[] = 'Falta el parámetro op2';
-    }
-    if (isset($_GET['op'])) {
-        $op = trim($_GET['op']);
-    } else {
-        $error[] = 'Falta el parámetro op';
-    }
-    if (empty($error)) {
-        $res = calcular_resultado($op1, $op2, $op);
-        mostrar_resultado($op1, $op2, $op, $res);
-    } else {
-        mostrar_errores($error);
-    }
+    
     ?>
 
     <form action="" method="get">
         <div>
             <label>
                 Primer operando:
-                <input type="text" name="op1">
+                <input type="text" name="op1" value="<?= $op1 ?>">
             </label>
         </div>
         <div>
             <label>
                 Segundo operando:
-                <input type="text" name="op2">
+                <input type="text" name="op2" value="<?= $op2 ?>">
             </label>
         </div>
         <div>
             <label>
                 Operación:
-                <input type="text" name="op">
+                <input type="text" name="op" value="<?= $op ?>">
             </label>
         </div>
         <button type="submit">Calcular</button>
